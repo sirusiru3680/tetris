f38fe60474fddff0dcfec9c81949da118be62a1f
sirusiru3680 <135177116+sirusiru3680@users.noreply.github.com>
2023-06-02 13:59:58 +0900

    Update tetrismain.c

--- tetrismain.c
+++ tetrismain.c
@@ -265,8 +265,12 @@ int board[15 + 1][10 + 2] = {
 };
 
 int block_x, block_y;
-int random_shape =0;
+int random_shape = 0;
 int rotate = 0;
+int preview_shape = 0;
+int keep_shape = -1;
+int swap_shape = -1; //모양 임시 저장 변수 
+int score = 0;
 
 void gotoxy(int x, int y) {
 	COORD pos = { x,y };
@@ -278,6 +282,7 @@ int WallCrashEvent(int);
 void SetMouseCurSorInv();
 void AddBlock();
 void DrawBoard();
+void PlaceBlock();
 void DropBlock();
 void CheckLine();
 
@@ -285,7 +290,7 @@ void CheckLine();
 int main(void) {
 	int dropbasetime = (int)time(NULL);
 	int droptime = 0;
-	double delaytime = (double)time(NULL);
+	int delaytime = (int)time(NULL);
 
 	DrawBoard();
 
@@ -314,14 +319,42 @@ int main(void) {
 			if (WallCrashEvent(RIGHT) == 0) {
 				if (WallCrashEvent(LEFT) == 0) {
 					if (CrashEvent() == 0) {
-						otate = (rotate + 1) % 4;
+						rotate = (rotate + 1) % 4;
 						DrawBoard();
-						delaytime = (double)time(NULL);
 					}
 				}
-			
+			}
 		}
-
+		if (GetAsyncKeyState(VK_SPACE) & 0x8000) { //한번에 떨어트리기
+			if ((int)time(NULL) - delaytime >= 1) {
+				while(1) {
+					DropBlock();
+					if (CrashEvent() == 1) {
+						PlaceBlock();
+						DrawBoard();
+						break;
+					}
+				}
+				delaytime = (int)time(NULL);
+			}
+		}
+		
+		if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
+			if ((int)time(NULL) - delaytime >= 1) {
+				if (keep_shape == -1) {
+					keep_shape = random_shape;
+					AddBlock();
+					DrawBoard();
+				}
+				else {
+					swap_shape = random_shape;
+					random_shape = keep_shape;
+					keep_shape = swap_shape;
+					DrawBoard();
+				}
+				delaytime = (int)time(NULL);
+			}
+		}
 
 		droptime = (int)time(NULL) - dropbasetime;
 		if (droptime >= 1) {
@@ -382,28 +415,57 @@ void SetMouseCurSorInv() {
 }
 
 void AddBlock() {
+	rotate = 0;
 	block_x = INDEX(5);
 	block_y = INDEX(1);
 	srand((int)time(NULL));
-	random_shape = rand() % 7;
+	random_shape = preview_shape;
+	preview_shape = rand() % 8;
 }
 
 void DrawBoard() {
 	system("cls");
 	for (int pos_y = 0; pos_y < 16; pos_y++) {
 		for (int pos_x = 0; pos_x < 12; pos_x++) {
-			gotoxy(6 + (pos_x * 2), 6 + pos_y);
+			gotoxy(16 + (pos_x * 2), 6 + pos_y);
 			printf("%s", ((board[pos_y][pos_x] == 1) ? "■" : "□"));
 		}
 	}
 	for (int pos_y = 0; pos_y < 4; pos_y++) {
 		for (int pos_x = 0; pos_x < 4; pos_x++) {
-			gotoxy(6 + (block_x+pos_x)*2, 6 + block_y + pos_y);
+			gotoxy(16 + (block_x+pos_x)*2, 6 + block_y + pos_y);
 			if (block[random_shape][rotate][pos_y][pos_x] == 1) {
 				printf("■");
 			}
 		}
 	}
+	
+	gotoxy(44, 6);
+	printf("다음 블록");
+	for (int pos_y = 0; pos_y < 4; pos_y++) {
+		for (int pos_x = 0; pos_x < 4; pos_x++) {
+			gotoxy(44 + pos_x*2, 7 + pos_y);
+			if (block[preview_shape][0][pos_y][pos_x] == 1) {
+				printf("■");
+			}
+		}
+	}
+	
+	gotoxy(1, 6);
+	printf("저장된 블록");
+	for (int pos_y = 0; pos_y < 4; pos_y++) {
+		for (int pos_x = 0; pos_x < 4; pos_x++) {
+			gotoxy(2 + pos_x*2, 7 + pos_y);
+			if (block[keep_shape][0][pos_y][pos_x] == 1) {
+				printf("■");
+			}
+		}
+	}
+	
+
+	
+	gotoxy(44, 4);
+	printf("점수: %d", score);
 
 	gotoxy(0, 6 + 16);
 }
@@ -450,6 +512,7 @@ void CheckLine() {
 			for (int pos_x = 0; pos_x < 10; pos_x++) {
 				board[0][pos_x] = 0;
 			}
+			score+=100;
 		}
 		count = 0;
 	}
